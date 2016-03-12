18beff4f4b21303fefee298eaf592c847ea3df80
Ethan Durrant <emd519@yahoo.com>
2016-03-12 15:48:14 -0700

    defined the japanese katakana and symbol codes

--- avr/timelineBanner/LCD.h
+++ avr/timelineBanner/LCD.h
@@ -2,4 +2,14 @@
 #ifndef LCD_H
 #define LCD_H
 
+#define LCD_D_DDR   DDRB
+#define LCD_D_PORT  PORTB
+#define LCD_RS_DDR  DDRD
+#define LCD_RS_PORT PORTD
+#define LCD_RS_BIT  PD5
+#define LCD_E_DDR   DDRD
+#define LCD_E_PORT  PORTD
+#define LCD_E_BIT   PD6
+
+
 #endif
---
+++ avr/timelineBanner/LCD_char_jp.h
@@ -0,0 +1,104 @@
+
+
+#ifndef LCD_CHAR_JP_H
+#define LCD_CHAR_JP_H
+
+
+//definitions for the japanese characters
+
+//symbols, punctuiation, and Small Kana
+#define CHAR_JP_SPACE   0xA0
+#define CHAR_JP_FULL_STOP    0xA1
+#define CHAR_JP_PERIOD  CHAR_JP_FULL_STOP
+#define CHAR_JP_L_BRACKET   0xA2
+#define CHAR_JP_R_BRACKET   0xA3
+#define CHAR_JP_PAUSE   0xA4
+#define CHAR_JP_DOT     0xA5
+#define CHAR_JP_WO      0xA6
+#define CHAR_JP_Sm_A    0xA7
+#define CHAR_JP_Sm_I    0xA8
+#define CHAR_JP_Sm_U    0xA9
+#define CHAR_JP_Sm_E    0xAA
+#define CHAR_JP_Sm_O    0xAB
+#define CHAR_JP_Sm_YA   0xAC
+#define CHAR_JP_Sm_YU   0xAD
+#define CHAR_JP_Sm_YO   0xAE
+#define CHAR_JP_Sm_TSU  0xAF
+
+#define CHAR_JP_PROLONG 0xB0
+#define CHAR_JP_LONG    CHAR_JP_PROLONG
+#define CHAR_JP_DASH CHAR_JP_PROLONG
+
+//Full Size Katakana
+#define CHAR_JP_A       0xB1
+#define CHAR_JP_I       0xB2
+#define CHAR_JP_U       0xB3
+#define CHAR_JP_E       0xB4
+#define CHAR_JP_O       0xB5
+
+#define CHAR_JP_KA      0xB6
+#define CHAR_JP_KI      0xB7
+#define CHAR_JP_KU      0xB8
+#define CHAR_JP_KE      0xB9
+#define CHAR_JP_KO      0xBA
+
+#define CHAR_JP_SA      0xBB
+#define CHAR_JP_SHI     0xBC
+#define CHAR_JP_SU      0xBD
+#define CHAR_JP_SE      0xBE
+#define CHAR_JP_SO      0xBF
+
+#define CHAR_JP_TA      0xC0
+#define CHAR_JP_CHI     0xC1
+#define CHAR_JP_TSU     0xC2
+#define CHAR_JP_TE      0xC3
+#define CHAR_JP_TO      0xC4
+
+#define CHAR_JP_NA      0xC5
+#define CHAR_JP_NI      0xC6
+#define CHAR_JP_NU      0xC7
+#define CHAR_JP_NE      0xC8
+#define CHAR_JP_NO      0xC9
+
+#define CHAR_JP_HA      0xCA
+#define CHAR_JP_HI      0xCB
+#define CHAR_JP_HU      0xCC
+#define CHAR_JP_FU      CHAR_JP_HU
+#define CHAR_JP_HE      0xCD
+#define CHAR_JP_HO      0xCE
+
+#define CHAR_JP_MA      0xCF
+#define CHAR_JP_MI      0xD0
+#define CHAR_JP_MU      0xD1
+#define CHAR_JP_ME      0xD2
+#define CHAR_JP_MO      0xD3
+
+#define CHAR_JP_YA      0xD4
+#define CHAR_JP_YU      0xD5
+#define CHAR_JP_YO      0xD6
+
+#define CHAR_JP_RA      0xD7
+#define CHAR_JP_RI      0xD8
+#define CHAR_JP_RU      0xD9
+#define CHAR_JP_RE      0xDA
+#define CHAR_JP_RO      0xDB
+
+#define CHAR_JP_WA      0xDC
+
+#define CHAR_JP_N       0xDD
+
+//the Dakuten
+#define CHAR_JP_DAKUTEN 0xDE
+#define CHAR_JP_NIGORI  CHAR_JP_DAKUTEN
+#define CHAR_JP_HANDAKUTEN  0xDF
+#define CHAR_JP_MARU    CHAR_JP_HANDAKUTEN
+
+//mini kanji
+#define CHAR_JP_SEN     0xFA
+#define CHAR_JP_MAN     0xFB
+#define CHAR_JP_EN      0xFC
+
+
+
+
+#endif
