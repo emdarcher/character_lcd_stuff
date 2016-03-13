acd7b7c7cde39e6e3cae06d587e55df8229e9c40
Ethan Durrant <emd519@yahoo.com>
2016-03-12 18:49:04 -0700

    displaying first line

--- avr/timelineBanner/main.c
+++ avr/timelineBanner/main.c
@@ -6,18 +6,22 @@
 #include "LCD.h"
 #include "LCD_char_jp.h"
 
-const uint8_t line1[] PROGMEM = {'A', 'b', 'c', 
-    CHAR_JP_TE, CHAR_JP_SU, CHAR_JP_TO};
+const uint8_t line1[] PROGMEM = {
+    //i-san イーサン 
+    CHAR_JP_I, CHAR_JP_PROLONG, CHAR_JP_SA, CHAR_JP_N,
+    //(dot) ・
+    CHAR_JP_DOT,
+    //duranto デュラント 
+    CHAR_JP_TE, CHAR_JP_NIGORI, CHAR_JP_Sm_YU, 
+    CHAR_JP_RA, CHAR_JP_N, CHAR_JP_TO,
+    ' ',
+    //no ノ (の)
+    CHAR_JP_NO,
+};
 
 int main(void){
 
     lcd_init();
-    //lcd_send_char('A');
-    //lcd_send_char('b');
-    //lcd_send_char('c');    
-    //lcd_send_char(CHAR_JP_TE);
-    //lcd_send_char(CHAR_JP_SU);
-    //lcd_send_char(CHAR_JP_TO);
     lcd_send_arr_P(&line1, sizeof(line1));
 
     while(1);
