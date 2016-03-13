44ed890434434ea30ea4acb8e939c4f21bfdbca9
Ethan Durrant <emd519@yahoo.com>
2016-03-12 17:58:59 -0700

    got basic commands and characters working!

--- avr/timelineBanner/LCD.c
+++ avr/timelineBanner/LCD.c
@@ -3,4 +3,92 @@
 #include "LCD.h"
 
 
+void lcd_init(void){
+    //setup pins
+    LCD_D_DDR |= 0xff;
+    LCD_D_PORT = 0x00;
+
+    //init RS
+    LCD_RS_DDR |= (1<<LCD_RS_BIT);
+    //set to 0
+    LCD_RS_PORT &= ~(1<<LCD_RS_BIT); 
+
+    //init E
+    LCD_E_DDR |= (1<<LCD_E_BIT);
+    //set to 0
+    LCD_E_PORT &= ~(1<<LCD_E_BIT);
+
+    //wait some time for the LCD to fully start up
+    _delay_ms(20); 
+
+    //set display on with underline and blinking cursor
+    lcd_send_cmd(LCD_CMD_DISP_CTRL
+                    | LCD_CMD_DISP_CTRL_DISP_ON
+                    | LCD_CMD_DISP_CTRL_UL_ON
+                    | LCD_CMD_DISP_CTRL_BLINK_ON); 
+
+    //set up the function of the display
+    //for 8-bit interface, 2 lines, 5x7 font
+    lcd_send_cmd(LCD_CMD_FUNC_SET
+                    | LCD_CMD_FUNC_SET_8BIT
+                    | LCD_CMD_FUNC_SET_2LINE
+                    | LCD_CMD_FUNC_SET_2LINE);
+    
+    lcd_send_cmd(LCD_CMD_RET_HOME);
+    
+    _delay_ms(10);
+}
+void lcd_send_cmd(uint8_t in_cmd){
+    //clear the data pins
+    LCD_D_PORT = 0x00;
+    //clear enable
+    LCD_E_PORT &= ~(1<<LCD_E_BIT);
+
+    //set to cmd mode
+    LCD_RS_PORT &= ~(1<<LCD_RS_BIT); 
+   
+    //write the cmd to the data pins
+    LCD_D_PORT = in_cmd;
+
+    //set enable
+    LCD_E_PORT |= (1<<LCD_E_BIT);
+    //wait a bit
+    _delay_us(1);
+    //clear enable to read in the data
+    LCD_E_PORT &= ~(1<<LCD_E_BIT);
+    //now clear data pins
+    LCD_D_PORT = 0x00;
+
+    //wait
+    _delay_us(40);
+}
+void lcd_send_char(uint8_t in_ch){
+    //clear the data pins
+    LCD_D_PORT = 0x00;
+    //clear enable
+    LCD_E_PORT &= ~(1<<LCD_E_BIT);
+   
+    //set to character mode
+    LCD_RS_PORT |= (1<<LCD_RS_BIT); 
+
+    //write the character to the data pins
+    LCD_D_PORT = in_ch;
+
+    //set enable
+    LCD_E_PORT |= (1<<LCD_E_BIT);
+    //wait a bit
+    _delay_us(1);
+    //clear enable to read in the data
+    LCD_E_PORT &= ~(1<<LCD_E_BIT);
+    //now clear data pins
+    LCD_D_PORT = 0x00;
+
+
+    //wait
+    _delay_us(40);
+
+}
+void lcd_send_arr(uint8_t * in_arr){
+
+}
 
--- avr/timelineBanner/LCD.h
+++ avr/timelineBanner/LCD.h
@@ -2,6 +2,10 @@
 #ifndef LCD_H
 #define LCD_H
 
+#include <avr/io.h>
+#include <util/delay.h>
+
+
 #define LCD_D_DDR   DDRB
 #define LCD_D_PORT  PORTB
 #define LCD_RS_DDR  DDRD
@@ -10,6 +14,49 @@
 #define LCD_E_DDR   DDRD
 #define LCD_E_PORT  PORTD
 #define LCD_E_BIT   PD6
+
+//define the commands
+#define LCD_CMD_CLR_DISP    0x01
+
+#define LCD_CMD_RET_HOME    0x02
+
+#define LCD_CMD_ENTRY_MODE_SEL  0x04
+#define LCD_CMD_ENTRY_MODE_SET_INC  0x02
+#define LCD_CMD_ENTRY_MODE_SET_DEC  0x00
+#define LCD_CMD_ENTRY_MODE_SET_SHIFT    0x01
+
+#define LCD_CMD_DISP_CTRL   0x08
+#define LCD_CMD_DISP_CTRL_DISP_ON   0x04
+#define LCD_CMD_DISP_CTRL_DISP_OFF  0x00
+#define LCD_CMD_DISP_CTRL_UL_ON     0x02
+#define LCD_CMD_DISP_CTRL_UL_OFF    0x00
+#define LCD_CMD_DISP_CTRL_BLINK_ON  0x01
+#define LCD_CMD_DISP_CTRL_BLINK_OFF 0x00
+
+#define LCD_CMD_CUR_DISP_SHIFT      0x10
+#define LCD_CMD_CUR_DISP_SHIFT_DISP_SHIFT   0x08
+#define LCD_CMD_CUR_DISP_SHIFT_CUR_MOVE 0x00
+#define LCD_CMD_CUR_DISP_SHIFT_RIGHT    0x04
+#define LCD_CMD_CUR_DISP_SHIFT_LEFT     0x00
+
+#define LCD_CMD_FUNC_SET    0x20
+#define LCD_CMD_FUNC_SET_8BIT   0x10
+#define LCD_CMD_FUNC_SET_4BIT   0x00
+#define LCD_CMD_FUNC_SET_2LINE  0x08
+#define LCD_CMD_FUNC_SET_1LINE  0x00
+#define LCD_CMD_FUNC_SET_10FONT 0x04
+#define LCD_CMD_FUNC_SET_7FONT  0x00
+
+//you will have to OR the address with the cmd
+//6bit addr
+#define LCD_CMD_SET_CGRAM_ADDR  0x40
+//7bit addr
+#define LCD_CMD_SET_DDRAM_ADDR  0x80
+
+void lcd_init(void);
+void lcd_send_cmd(uint8_t in_cmd);
+void lcd_send_char(uint8_t in_ch);
+void lcd_send_arr(uint8_t * in_arr);
 
 
 #endif
